1035baa79dfb979f298682308ef7bccca9b04bcc
Lakshan Bandara <lakshan@orelit.com>
2022-03-19 10:24:26 +0530

    App.use middleware with path

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 
-// ***** Accessing and Setting properties to Request ***** //
-// Example route: http://localhost:8000/about-us
+// ***** App.use middleware with path ***** //
 app.use((req, res, next) => {
-    // Adding properties to request
     req.requestTime = Date.now();
-    // Accessing properties of request.
-    // Logger like morgan
     console.log(req.method, req.path);
+    next();
+});
+
+// App.use middleware with path
+app.use('/about-us', (req, res, next) => {
+    console.log("This middleware runs only for /about-us path");
     next();
 });
 
@@ -17,7 +19,6 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about-us', (req, res) => {
-    // Accessing requestTime sat by the custom middleware.
     console.log(`REQUEST TIME: ${req.requestTime}`);
     res.send("About us page");
 });
