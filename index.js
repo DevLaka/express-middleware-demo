6116345d12991978047e858158dd1a8794a94252
Lakshan Bandara <lakshan@orelit.com>
2022-03-19 10:51:50 +0530

    Example : Protecting specific routes

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 
-// ***** Example for app.use : 404 Route ***** //
+// ***** Example : Protecting specific routes ***** //
 app.use((req, res, next) => {
     req.requestTime = Date.now();
     console.log(req.method, req.path);
     next();
 });
+
+// Defining validateUser middleware
+const validateUser = (req, res, next) => {
+    const { password } = req.query;
+    if (password === 'devlaka') {
+        next();
+    }
+    res.send('PASSWORD REQUIRED!');
+};
 
 app.use('/about-us', (req, res, next) => {
     console.log("This middleware runs only for /about-us path");
@@ -22,7 +31,16 @@ app.get('/about-us', (req, res) => {
     res.send("About us page");
 });
 
-// Example for app.use : 404 Route
+// Use validateUser middleware for private route
+app.get('/private', validateUser, (req, res) => {
+    res.send("Private Data");
+});
+
+// Use validateUser middleware for top-secret route
+app.get('/top-secret', validateUser, (req, res) => {
+    res.send("Top Secret Data");
+});
+
 app.use((req, res) => {
     res.status(404).send('Requested Resource Not Found!');
 });
